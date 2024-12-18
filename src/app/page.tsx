82f898fadd1ad6660888d1177e8bc89033029235
jimini <jiminido1@gmail.com>
2024-12-19 02:39:13 +0900

    supabase on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+"use client"
+
+import { useEffect } from "react";
+import { supabase } from "../../utils/supabase";
+
 export default function Home() {
+  useEffect(() => {
+    supabase.from('test').select('*').then(console.log);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen w-full p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen w-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-5xl underline decoration-wavy decoration-red-700 underline-offset-8 font-extrabold mb-4 whitespace-nowrap">걍 내가 만드는 소리얼 예약 페이지</h1>
-      <div className="w-full overflow-x-auto justify-items-center">
-        <table className="w-75% border-collapse border border-gray-300">
+      <div className="w-[75%] justify-items-center">
+        <table className="w-full max-w-[75%] border-collapse border border-gray-300">
           <thead>
             <tr>
               {['일', '월', '화', '수', '목', '금', '토'].map((day) => (
-                <th key={day} className="border border-gray-300 p-2">{day}</th>
+                <th key={day} className="flex-1 border border-gray-300 p-2">{day}</th>
               ))}
             </tr>
           </thead>
@@ -21,7 +30,7 @@ export default function Home() {
                     {Array.from({ length: 12 }).map((_, hourIndex) => {
                       const hour = 11 + hourIndex; // Start from 11:00
                       return (
-                        <div key={hourIndex} className="flex border-b border-gray-200 last:border-b-0">
+                        <div key={hourIndex} className="flex-1 border-b border-gray-200 last:border-b-0">
                           <div className="w-16 p-1 text-xs border-r border-gray-200 bg-gray-50">
                             {hour}:00
                           </div>
