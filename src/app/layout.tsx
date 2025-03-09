97d96f55e62f28afdaec0542fbba5d14517d7fe9
jimini <jiminido1@gmail.com>
2025-03-10 00:23:57 +0900

    i no sup darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "소리얼 예약 페이지",
   description: "소리얼 예약방 대신에 만들었습니다",
-  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+  colorScheme: "light",
 };
 
 export default function RootLayout({
